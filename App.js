403207985ede5e19cf299ea7662aac5f1272e8d2
IsuruRanapana <113498635+IsuruRanapana@users.noreply.github.com>
2022-09-14 17:59:02 +0530

    React Native Animal Application finished

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import animals from "./assets/img";
+import { Button } from "./components";
 
 export default function App() {
+  const [currentAnimal, setCurrentAnimal] = useState(animals.cat);
+  const [currentAnimalName, setCurrentAnimalName] = useState("Kitty");
+
+  const onPress = ({ animal }) => {
+    switch (animal) {
+      case "Cat": {
+        setCurrentAnimal(animals.cat);
+        setCurrentAnimalName("Kitty");
+        break;
+      }
+      case "Dog": {
+        setCurrentAnimal(animals.dog);
+        setCurrentAnimalName("Doggy");
+        break;
+      }
+      default: {
+        setCurrentAnimal(animals.rabbit);
+        setCurrentAnimalName("Bunny");
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hi, {currentAnimalName}...</Text>
+      <Image source={currentAnimal} style={styles.image} />
+      <View style={styles.buttons}>
+        <Button
+          onPress={() => onPress({ animal: "Cat" })}
+          title={"Cat"}
+          image={animals.icCat}
+        />
+        <Button
+          onPress={() => onPress({ animal: "Dog" })}
+          title={"Dog"}
+          image={animals.icDog}
+        />
+        <Button onPress={onPress} title={"Rabbit"} image={animals.icRabbit} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +52,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    height: 250,
+    width: 250,
+    borderRadius: 8,
+  },
+  buttons: {
+    marginTop: 50,
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-evenly",
+  },
+  title: {
+    fontSize: 25,
+    marginBottom: 20,
+    fontWeight: "bold",
   },
 });
---
+++ assets/img/index.js
@@ -0,0 +1,15 @@
+import icCat from "./ic_cat.png";
+import icDog from "./ic_dog.png";
+import icRabbit from "./ic_rabbit.png";
+import cat from "./cat.webp";
+import rabbit from "./rabbit.webp";
+import dog from "./dog.jpg";
+
+export default {
+  icCat,
+  icDog,
+  icRabbit,
+  cat,
+  dog,
+  rabbit,
+};
